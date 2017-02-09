cfddfcd7217a2674b47b91fa35ce3fd19be8ac50
micmiu <sjsky007@gmail.com>
2017-02-09 13:38:11 +0800

    Add:urldecode

---
+++ java-tutorial/src/test/java/com/micmiu/tutorial/java/URLDecode.java
@@ -0,0 +1,27 @@
+package com.micmiu.tutorial.java;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URLDecoder;
+
+/**
+ * Created
+ * User: <a href="http://micmiu.com">micmiu</a>
+ * Date: 12/30/2014
+ * Time: 16:20
+ */
+public class URLDecode {
+
+	private static final String[] encodes = new String[]{"UTF-8", "GBK", "GB2312", "ISO-8859-1", "UTF-16BE", "UTF-16LE", "UTF-16"};
+
+	public static void main(String[] args) throws Exception {
+		String str = "%AF%AB%CB%00%00%00%00%20%94%E1%E8%81%00%00%00%01%00%00%00%00%00%00%00%01%00%00%00%08RootPOA%00%00%00%00%08%00%00%00%01%00%00%00%00%14";
+
+		for (String encode : encodes) {
+			String ret = URLDecoder.decode(str, encode);
+			System.out.println(encode + " --> " + ret);
+		}
+
+	}
+}
